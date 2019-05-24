f080d9caba242c0285872d6a15c4e5afe9f9311d
jshiers-certik <shiers.jacob@certik.org>
2019-05-24 17:06:00 -0400

    updated

--- keeper.go
+++ keeper.go
@@ -5,17 +5,63 @@ package nameservice
 //and contains most of the core functionality of a module
 
 import(
-	"github.com/cosmos.cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos.cosmos-sdk/codec"  //provides tools to work with the Cosmos encoding format
+	"github.com/cosmos/cosmos-sdk/x/bank"  //controls accounts and coin transfers
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types" //contains commonly used types throughout SDK
+
 
 )
 
 //Keeper maintains the link to data storage and exposes getter/setter
 //methods for various parts of the state machine
-type Keeper struct{
-	coinKeeper bank.Keeper
+type Keeper struct {
+	coinKeeper bank.Keeper //reference to the Keeper from the "bank" module.
+	//including it allows code in this module to call functions
+	//from the "bank" module.
 	storeKey sdk.Storekey //unexposed key to access store from sdk.Context
-	cdc *codec.Codec //The wire codec for binary encoding/encoding
+	cdc      *codec.Codec //The wire codec for binary encoding/decoding. It's a pointer
+	 						//to the codec that is used by Amino t encode and decode
+	 						//binary structs
 }
+
+//sets the entire Whois metadata struct for a name
+func (k Keeper) SetWhois(ctx sdk.Context, name string, whois Whois){
+	if whois.Owner.Empty(){
+		return
+	}
+	store:=ctx.KVStore(k.storekey)
+	store.Set([]byte(name), k.cdc.MustMarshalBinaryBare(whois))
+
+}
+
+func (k Keeper) GetWhois(ctx sdk.Context, name string) Whois {
+
+	store:=ctx.KVStore(k.storekey)
+	if !store.Has([]byte(name)) {
+		return NewWhois()
+	}
+	bz:=store.Get([]byte(name))
+	var whois Whois
+	k.cdc.MustUnmarshalBinaryBare(bz, &whois)
+	return whois
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
--- types.go
+++ types.go
@@ -23,3 +23,4 @@ func NewWhois() Whois{
 		Price: MinNamePrice,
 	}
 }
+
